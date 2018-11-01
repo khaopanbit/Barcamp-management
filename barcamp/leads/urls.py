1a55b396eaeb32f2d6e8a14fb14357ea9508560c
ThanaphonKeawjam <tongasura001@gmail.com>
2018-11-02 01:57:41 +0700

    clear

--- barcamp/leads/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/topic/', views.TopicListCreate.as_view()),
-    path('api/user/', views.UserListCreate.as_view()),
-]
