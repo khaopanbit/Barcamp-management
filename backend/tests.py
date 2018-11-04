5bc063a2530290703650538d1c0b559328ed0550
ThanaphonKeawjam <tongasura001@gmail.com>
2018-11-05 00:29:41 +0700

    add test backend

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Topics
 
-# Create your tests here.
+class TopicModelTests(TestCase):
+
+    def test_string_representation(self):
+        topic = Topics(topic_name='BNK', speaker='Jobsan')
+
+        self.assertEqual(str(topic), 'BNK by Jobsan')
