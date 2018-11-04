3e2dadbe1802cc28e619084332f096a057fa29d4
JPSKE15 <hokiz_1999@hotmail.com>
2018-11-04 22:49:10 +0700

    user can add new topic and window display all topic

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics,status
+from rest_framework.response import Response
 from .serializers import UserSerializer, TopicSerializer
 from .models import User, Topics
 
--- frontend/src/components/Speaker.js
+++ frontend/src/components/Speaker.js
@@ -1,8 +1,9 @@
 import React ,{ Component } from 'react';
 import firebase from 'firebase'
 import TimeKeeper from 'react-timekeeper';
-import {ButtonGroup, Button,Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, UncontrolledDropdown,
+import {Modal,ModalHeader, ModalBody, ModalFooter, ButtonGroup, Button,Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, UncontrolledDropdown,
         DropdownToggle, DropdownMenu, DropdownItem,Popover, PopoverBody, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { Timepicker } from 'react-timekeeper/lib/components/Timepicker';
 
 const Logo = {
     fontFamily: 'cursive',
@@ -43,17 +44,31 @@ class Speaker extends Component {
     this.logout = this.logout.bind(this);
     this.addNewTopic = this.addNewTopic.bind(this);
     this.Popovertoggle = this.Popovertoggle.bind(this);
-    this.handleStartTimeChange = this.handleStartTimeChange.bind(this)
-    this.handleStopTimeChange = this.handleStopTimeChange.bind(this)
-    this.toggleStartTimekeeper = this.toggleStartTimekeeper.bind(this)
-    this.toggleStopTimekeeper = this.toggleStopTimekeeper.bind(this)
+    this.handleStartTimeChange = this.handleStartTimeChange.bind(this);
+    this.handleStopTimeChange = this.handleStopTimeChange.bind(this);
+    this.toggleStartTimekeeper = this.toggleStartTimekeeper.bind(this);
+    this.toggleStopTimekeeper = this.toggleStopTimekeeper.bind(this);
+    this.handleTopicChange = this.handleTopicChange.bind(this);
+    this.handleDesChange = this.handleDesChange.bind(this);
+    this.change = this.change.bind(this);
+    this.getData = this.getData.bind(this);
+    this.modalToggle = this.modalToggle.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
     this.state = {
       isOpen: false,
+      modal: [] ,
       popoverOpen: false,
-      startTime: '6:50 am',
       displayStartTimepicker: false,
+      displayStopTimepicker2: false,
+      allTopic : [] ,
+      topic : "No topic",
+      description : "No des.",
+      startTime: '6:50 am',
       stopTime: '6:50 pm',
-      displayStopTimepicker2: false
+      name : "Speaker",
+      room : 0 ,
+      vote : 0 ,
+      start: true
     };
   }
   toggle() {
@@ -82,15 +97,75 @@ class Speaker extends Component {
   toggleStopTimekeeper(val){
     this.setState({displayStopTimepicker: val})
   }
-  addNewTopic(){
-    this.setState({
-        popoverOpen: !this.state.popoverOpen
-      });
+  handleTopicChange(e) {
+    this.setState({ topic: e.target.value });
   }
+  handleDesChange(e) {
+    this.setState({ description: e.target.value });
+  }
+  modalToggle(id) {
+    this.state.modal[id] = !this.state.modal[id]
+    this.forceUpdate()
+    console.log('modal')
+  }
+  addNewTopic(){
+    var to = this.state.topic
+    var des = this.state.description
+    var stime = this.state.startTime
+    var etime = this.state.stopTime
+    var speak = this.state.name
+    var r = this.state.room
+    var v = this.state.vote
+    const sendData = { 'topic_name' : to, 'description':des, 'start_time':stime, 'end_time':etime, 'speaker':speak, 'room':r, 'vote':v}
+    $.ajax({
+      dataType: 'json',
+      url: 'http://localhost:3000/api/topic/',
+      type: 'POST',
+      data: JSON.stringify(sendData),
+      contentType:'application/json',
+    })
+    this.setState({
+        popoverOpen: !this.state.popoverOpen,
+      });
+    console.log(this.state.topic)
+    console.log(this.state.description)
+    window.location.reload();
+  }
+  getData(){
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/api/topic/",
+      dataType: "json",
+      success: function () {
+        console.log('success')
+      }
+    });
+  }
+  change(){
+    if(this.state.start == true){
+    for(var i = this.state.start ; i <= this.state.allTopic.length;i++){
+      console.log('lol')
+      var newArray = this.state.modal.slice();    
+      newArray.push({id: false});   
+      this.setState({modal:newArray})
+    }
+    this.setState({start:false})
+  }
+}
+  componentDidMount() {
+    fetch("http://localhost:3000/api/topic/")
+      .then(response => {
+        if (response.status !== 200) {
+          return console.log('error')
+        }
+        return response.json();
+      })
+      .then(data => this.setState({ allTopic: data }));
+    console.log('initialize')
+}
   render() {
     return (
       <div>
-
         <div>
         <Navbar color="light" light expand="md" style={HeaderTap} >
           <NavbarBrand >SPEAKER</NavbarBrand>
@@ -99,7 +174,7 @@ class Speaker extends Component {
             <Nav className="ml-auto" navbar>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                {firebase.auth().currentUser.displayName}
+                {/* {firebase.auth().currentUser.displayName} */}
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem style={{color: 'red'}} onClick={this.logout}>
@@ -120,12 +195,12 @@ class Speaker extends Component {
             
             <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
           <PopoverBody style={popoverBody}>
-            <InputGroup>
+            <InputGroup >
                 <InputGroupAddon addonType="prepend">Topic</InputGroupAddon>
-                <Input placeholder="topic" />
+                <Input placeholder="topic" onChange = {this.handleTopicChange} />
             </InputGroup>
             <br/>
-            <Input type="textarea" name="text" id="exampleText" placeholder="Description..."/>
+            <Input type="textarea" name="text" id="exampleText" placeholder="Description..." onChange = {this.handleDesChange}/>
             <br/>
             <ButtonGroup>
                 <Button style={StartTimeButton} onClick={()=>this.toggleStartTimekeeper(true)}>start : {this.state.startTime}</Button>
@@ -165,6 +240,19 @@ class Speaker extends Component {
           </PopoverBody>
         </Popover>
         </div>
+        <div>
+            {this.state.allTopic.map((topic, index) => (
+              <div>
+                <Button color="danger" onClick={() =>this.modalToggle(index+1)}>{topic.topic_name}</Button>
+                <Modal isOpen={this.state.modal[index+1]} toggle={() =>this.modalToggle(index+1)} >
+                  <ModalHeader toggle={() =>this.modalToggle(index+1)} charCode= "x">{topic.topic_name}</ModalHeader>
+                  <ModalBody>{topic.description}</ModalBody>
+                  <ModalFooter>by {topic.speaker}</ModalFooter>
+                </Modal>
+              </div>
+            ))}
+            {this.change()}
+          </div>
       </div>
     );
   }
